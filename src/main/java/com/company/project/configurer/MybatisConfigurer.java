281495adec54a65131a194d1ab8e022aafe5184d
wangxian <xian366@126.com>
2019-05-04 11:18:37 +0800

    格式化代码，调整代码注释，升级 springboot 到 1.5.20 GA

--- src/main/java/com/company/project/configurer/MybatisConfigurer.java
+++ src/main/java/com/company/project/configurer/MybatisConfigurer.java
@@ -4,16 +4,12 @@ import com.github.pagehelper.PageHelper;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -31,18 +27,25 @@ public class MybatisConfigurer {
         factory.setDataSource(dataSource);
         factory.setTypeAliasesPackage(MODEL_PACKAGE);
 
-        //配置分页插件，详情请查阅官方文档
+        // 配置分页插件，详情请查阅官方文档
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
-        properties.setProperty("pageSizeZero", "true");//分页尺寸为0时查询所有纪录不再执行分页
-        properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
-        properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
+
+        // 分页尺寸为 0 时查询所有纪录不再执行分页
+        properties.setProperty("pageSizeZero", "true");
+
+        // 页码<=0 查询第一页，页码>=总页数查询最后一页
+        properties.setProperty("reasonable", "true");
+
+        // 支持通过 Mapper 接口参数来传递分页参数
+        properties.setProperty("supportMethodsArguments", "true");
+
         pageHelper.setProperties(properties);
 
-        //添加插件
+        // 添加插件
         factory.setPlugins(new Interceptor[]{pageHelper});
 
-        //添加XML目录
+        // 添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
         return factory.getObject();
@@ -54,10 +57,12 @@ public class MybatisConfigurer {
         mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
         mapperScannerConfigurer.setBasePackage(MAPPER_PACKAGE);
 
-        //配置通用Mapper，详情请查阅官方文档
+        // 配置通用Mapper，详情请查阅官方文档
         Properties properties = new Properties();
         properties.setProperty("mappers", MAPPER_INTERFACE_REFERENCE);
-        properties.setProperty("notEmpty", "false");//insert、update是否判断字符串类型!='' 即 test="str != null"表达式内是否追加 and str != ''
+
+        // insert、update是否判断字符串类型!='' 即 test="str != null"表达式内是否追加 and str != ''
+        properties.setProperty("notEmpty", "false");
         properties.setProperty("IDENTITY", "MYSQL");
         mapperScannerConfigurer.setProperties(properties);
 
--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -41,19 +41,26 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
-
     private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
-    @Value("${spring.profiles.active}")
-    private String env;//当前激活的配置文件
 
-    //使用阿里 FastJson 作为JSON MessageConverter
+    // 当前激活的配置文件
+    @Value("${spring.profiles.active}")
+    private String env;
+
+    // 使用阿里 FastJson 作为JSON MessageConverter
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
-        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);//保留空的字段
-        //SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
-        //SerializerFeature.WriteNullNumberAsZero//Number null -> 0
+
+        // 保留空的字段
+        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
+
+        // String null -> ""
+        // SerializerFeature.WriteNullStringAsEmpty,
+
+        // Number null -> 0
+        // SerializerFeature.WriteNullNumberAsZero
         // 按需配置，更多参考FastJson文档哈
 
         converter.setFastJsonConfig(config);
@@ -63,13 +70,14 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     }
 
 
-    //统一异常处理
+    // 统一异常处理
     @Override
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         exceptionResolvers.add(new HandlerExceptionResolver() {
             public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
                 Result result = new Result();
-                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                if (e instanceof ServiceException) {
+                    // 业务失败的异常，如 “账号或密码错误”
                     result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
                     logger.info(e.getMessage());
                 } else if (e instanceof NoHandlerFoundException) {
@@ -89,8 +97,10 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                     } else {
                         message = e.getMessage();
                     }
+
                     logger.error(message, e);
                 }
+
                 responseResult(response, result);
                 return new ModelAndView();
             }
@@ -98,21 +108,21 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         });
     }
 
-    //解决跨域问题
+    // 解决跨域问题
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        //registry.addMapping("/**");
+        registry.addMapping("/**");
     }
 
-    //添加拦截器
+    // 添加拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
+        // 接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
         if (!"dev".equals(env)) { //开发环境忽略签名认证
             registry.addInterceptor(new HandlerInterceptorAdapter() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                    //验证签名
+                    // 验证签名
                     boolean pass = validateSign(request);
                     if (pass) {
                         return true;
@@ -134,6 +144,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Content-type", "application/json;charset=UTF-8");
         response.setStatus(200);
+
         try {
             response.getWriter().write(JSON.toJSONString(result));
         } catch (IOException ex) {
@@ -148,27 +159,48 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
      * 3. 混合密钥（secret）进行md5获得签名，与请求的签名进行比较
      */
     private boolean validateSign(HttpServletRequest request) {
-        String requestSign = request.getParameter("sign");//获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        // 获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        String requestSign = request.getParameter("sign");
+
         if (StringUtils.isEmpty(requestSign)) {
             return false;
         }
+
         List<String> keys = new ArrayList<String>(request.getParameterMap().keySet());
-        keys.remove("sign");//排除sign参数
-        Collections.sort(keys);//排序
+
+        // 排除sign参数
+        keys.remove("sign");
+
+        // 排序
+        Collections.sort(keys);
 
         StringBuilder sb = new StringBuilder();
         for (String key : keys) {
-            sb.append(key).append("=").append(request.getParameter(key)).append("&");//拼接字符串
+            // 拼接字符串
+            sb.append(key).append("=").append(request.getParameter(key)).append("&");
         }
+
         String linkString = sb.toString();
-        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);//去除最后一个'&'
 
-        String secret = "Potato";//密钥，自己修改
-        String sign = DigestUtils.md5Hex(linkString + secret);//混合密钥md5
+        //去除最后一个'&'
+        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);
 
-        return StringUtils.equals(sign, requestSign);//比较
+        // 密钥，自己修改
+        String secret = "YOURS SECRET";
+
+        // 混合密钥md5
+        String sign = DigestUtils.md5Hex(linkString + secret);
+
+        // 比较sign
+        return StringUtils.equals(sign, requestSign);
     }
 
+    /**
+     * 获取客户端IP
+     *
+     * @param request 原始请求
+     * @return
+     */
     private String getIpAddress(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
@@ -186,6 +218,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
+
         // 如果是多级代理，那么取第一个ip为客户端ip
         if (ip != null && ip.indexOf(",") != -1) {
             ip = ip.substring(0, ip.indexOf(",")).trim();
